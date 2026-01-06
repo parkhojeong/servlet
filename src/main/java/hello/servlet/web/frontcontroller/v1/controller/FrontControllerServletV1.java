cdbaa362a13e9e4f58ce5011a9c2823172b38b0f
hj <parkhj062@gmail.com>
2026-01-06 20:25:23 +0900

    [31. 프론트 컨트롤러 도입 - v1]
    - introduce front controller
    - mapping url to controller

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/FrontControllerServletV1.java
@@ -0,0 +1,34 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ControllerV1 controller = controllerMap.get(req.getRequestURI());
+        if(controller == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        controller.process(req, resp);
+    }
+}
