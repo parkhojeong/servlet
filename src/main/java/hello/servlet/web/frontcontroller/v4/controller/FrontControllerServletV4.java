d6d6ccaae6143c0e2fdf9374708dc184c305ec34
hj <parkhj062@gmail.com>
2026-01-06 21:35:06 +0900

    [34. 단순하고 실용적인 컨트롤러 - v4]
    - controller use model parameter
    - controller return logical view name

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/FrontControllerServletV4.java
@@ -0,0 +1,55 @@
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV4() {
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ControllerV4 controller = controllerMap.get(req.getRequestURI());
+        if(controller == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        Map<String, String> paramMap = createParamMap(req);
+        Map<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        MyView myView = viewResolver(viewName);
+        myView.render(model ,req, resp);
+    }
+
+    private static MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private static @NonNull Map<String, String> createParamMap(HttpServletRequest req) {
+        Map<String, String> paramMap = new HashMap<>();
+        req.getParameterNames()
+                .asIterator()
+                .forEachRemaining(name -> paramMap.put(name, req.getParameter(name)));
+        return paramMap;
+    }
+}
